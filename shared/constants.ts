331bfc6a383950a3514f2b53708ed39d0a2deda0
idot_pepperi <ido.t@pepperi.com>
2023-01-01 17:26:32 +0200

    using alias audit_data_log_index

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    static AUDIT_DATA_LOG_INDEX = `audit_data_log`;
+    static AUDIT_DATA_LOG_INDEX = `audit_data_log_index`;
     static OPEN_DISTRO_ENDPOINT = `_opendistro/_sql`;
 }
