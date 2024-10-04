a8264ae2b8ed9d5cbea0d288db82d1b8fa375747
Izhar Ahmad <izharahmad@IN-KRQ7N4JV7Q.local>
2024-10-04 10:50:04 +0530

    added comment

--- client-side/src/app/components/async-jobs-block/async-job-audit-log-dialog/async-job-audit-log-dialog.component.ts
+++ client-side/src/app/components/async-jobs-block/async-job-audit-log-dialog/async-job-audit-log-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -10,7 +10,7 @@ export class AsyncJobAuditLogDialogComponent {
 
   constructor(
     private dialogRef: MatDialogRef<AsyncJobAuditLogDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {}
+    @Inject(MAT_DIALOG_DATA) public data: {} // data to be displayed
   ) {
   }
 
